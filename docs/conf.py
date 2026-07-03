47720d3284dce4efda18e99c2160b29bfb9bfa60
freesemt <freesemt@gmail.com>
2026-07-03 15:53:09 +0900

    docs: improve Sphinx conf.py (version, type hints, intersphinx, theme)

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,11 @@ project = 'Molass Library Legacy'
 copyright = '2025, The Molass Community'
 author = 'Molass Community'
 
+# Extract version from package
+from molass_legacy import get_version
+release = str(get_version(toml_only=True))  # Full version (e.g., "1.0.1")
+version = '.'.join(release.split('.')[:2])  # Short version (e.g., "1.0")
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
@@ -26,9 +31,23 @@ extensions = [
     "sphinx.ext.autosectionlabel",
     'sphinx_copybutton',
     'myst_parser',
+    'sphinx.ext.intersphinx',
 ]
 
 autoclass_content = 'both'
+
+# Type hints display
+autodoc_typehints = 'description'
+autodoc_typehints_description_target = 'documented'
+
+# Intersphinx mapping for cross-references
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'numpy': ('https://numpy.org/doc/stable', None),
+    'matplotlib': ('https://matplotlib.org/stable', None),
+    'scipy': ('https://docs.scipy.org/doc/scipy', None),
+}
+
 # napoleon_google_docstring = True
 # napoleon_numpy_docstring = True
 
@@ -44,6 +63,9 @@ add_module_names = False  # Removes the "molass." prefix from module names in ti
 html_theme_options = {
     "repository_url": "https://github.com/biosaxs-dev/molass-legacy",
     "use_repository_button": True,
+    "show_toc_level": 2,
+    "navigation_depth": 4,
+    "pygment_dark_style": "monokai",
 }
 
 html_theme = 'sphinx_book_theme'
